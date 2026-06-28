6c0b0071db971d281c696e0a52eea0b6476e0dc1
KarimTamer74 <kemotamer448@gmail.com>
2026-06-28 16:19:02 +0300

    fetch products with api done

--- lib/products_section.dart
+++ lib/products_section.dart
@@ -1,12 +1,41 @@
 // products_section.dart
 
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class ProductsSection extends StatelessWidget {
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:nova_app/products_grid_view.dart';
+
+class ProductsSection extends StatefulWidget {
   const ProductsSection({super.key});
 
   @override
+  State<ProductsSection> createState() => _ProductsSectionState();
+}
+
+class _ProductsSectionState extends State<ProductsSection> {
+  List products = [];
+
+  Future<void> getProducts() async {
+    final Dio dio = Dio();
+    final response = await dio.get('https://dummyjson.com/products');
+   products = response.data['products'];
+    
+    setState(() {});
+    log(products.length.toString());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getProducts();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    print("$height, $width");
     return Expanded(
       flex: 6,
       child: Column(
@@ -32,72 +61,7 @@ class ProductsSection extends StatelessWidget {
             ],
           ),
           Expanded(
-            child: GridView.builder(
-              itemCount: 20,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: .58,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-              ),
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Column(
-                    children: [
-                      Image.network(
-                        'https://d2u1z1lopyfwlx.cloudfront.net/thumbnails/fb75ddde-1b80-5d2b-8a5a-891b1136df4c/c7c301e6-fde2-5f81-aa42-1c3a30c8e606.jpg',
-                        height: 150,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Column(
-                          spacing: 5,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('ESSENCE', style: TextStyle(fontSize: 9)),
-                            Text(
-                              'Essence Mascara Lash Princess',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Row(
-                              spacing: 10,
-                              children: [
-                                Icon(Icons.star, color: Colors.orange),
-                                Text('2.6'),
-                              ],
-                            ),
-                            Row(
-                              spacing: 10,
-                              children: [
-                                Text(
-                                  '\$8.94',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  '\$9.99',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.red,
-                                    decoration: TextDecoration.lineThrough,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
+            child: ProductsGridView(products: products,)
           ),
         ],
       ),
