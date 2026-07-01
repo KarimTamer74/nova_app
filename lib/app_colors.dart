ee78efe545b5003175cc9544b7edf53cc9e3fbcd
KarimTamer74 <kemotamer448@gmail.com>
2026-07-01 08:53:49 +0300

    create app colors class to contain all nova app colors

---
+++ lib/app_colors.dart
@@ -0,0 +1,7 @@
+// app_colors.dart
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color buttonClr = Color(0xff2563EB);
+  // static const Color elvatedButtonClr = Color(0xFF000000);
+}
